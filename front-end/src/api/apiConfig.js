39c5630e2ad541ce790bb61f91bb7538e70e1455
Hong Xu <tsujonathan@gmail.com>
2017-05-02 19:56:47 -0400

    remove unused files

--- front-end/src/api/apiConfig.js
+++ front-end/src/api/apiConfig.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000/api/'; //'http://172.27.160.34:5000/api/';
-export const WS_BASE_URL = 'ws://localhost:5000/ws'; //'ws://172.27.160.34:5000/ws';
+export const BASE_URL = 'http://localhost:58124/api/'; // 'http://localhost:5000/api/';//'http://172.27.160.34:5000/api/';
+export const WS_BASE_URL = 'ws://localhost:58124/ws'; // 'ws://localhost:5000/ws';//'ws://172.27.160.34:5000/ws';
